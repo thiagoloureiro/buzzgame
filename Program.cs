99d0f888e891aa75575cc5cd44d393690f183f12
Thiago Loureiro <thiagoguaru@me.com>
2019-11-29 23:41:11 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                var buzzes = Check7and4(i);
+
+                if (i % 4 == 0)
+                {
+                    if (i / 4 == 4)
+                    {
+                        buzzes++;
+                    }
+
+                    buzzes++;
+                }
+
+                if (i % 7 == 0)
+                {
+                    if (i / 7 == 7)
+                    {
+                        buzzes++;
+                    }
+                    buzzes++;
+                }
+
+                SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+                synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Child);
+                synthesizer.Volume = 100;
+                synthesizer.Rate = 5;
+
+                if (buzzes > 0)
+                {
+                    //   Console.WriteLine(CreateBuzzString(i));
+                    synthesizer.Speak(CreateBuzzString(buzzes));
+                }
+                else
+                {
+                    synthesizer.Speak(i.ToString());
+                }
+            }
+        }
+
+        private static string CreateBuzzString(int buzzes)
+        {
+            string str = "";
+            for (int i = 0; i < buzzes; i++)
+            {
+                str += "buzz ";
+            }
+            return str;
+        }
+
+        private static int Check7and4(int number)
+        {
+            int contains = 0;
+
+            contains += number.ToString().Count(x => x == '4');
+            contains += number.ToString().Count(x => x == '7');
+
+            return contains;
+        }
+    }
+}
